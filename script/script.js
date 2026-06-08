9ea66537571db76dfbde035fa13e22f315b75428
robinraty <robinraty.dev@gmail.com>
2026-06-08 08:57:47 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,12 @@
+// =================================
+// 🌱 1. Sélection des éléments DOM
+// =================================
+
+const colors = ["red", "green", "blue", "yellow"]
+
+
+
+
+
+// function randomSuite : creation de la suite aleatoire par l'ordinateur.
+
